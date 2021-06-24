6d650c5a1291219f352e15f72a6899aafaf2b03a
Ivanlxw <leexianwei96@gmail.com>
2021-06-24 21:30:27 +0800

    all in same direction

--- strategy/multiple.py
+++ strategy/multiple.py
@@ -1,24 +1,37 @@
-from backtest.strategy.naive import Strategy
+from abc import ABC
+from trading_common.strategy.naive import Strategy
+from trading_common.utilities.enum import OrderPosition
 
-class MultipleAllStrategy(Strategy):
+
+class MultipleStrategy(ABC):
+    def order_same_dir(self, strategies):
+        return all(
+            strat is not None and (strat.signal_type == OrderPosition.BUY or strat.signal_type == OrderPosition.EXIT_SHORT) for strat in strategies
+        ) or all(
+            strat is not None and (strat.signal_type == OrderPosition.SELL or strat.signal_type == OrderPosition.EXIT_LONG) for strat in strategies
+        )
+
+
+class MultipleAllStrategy(MultipleStrategy, Strategy):
     def __init__(self, strategies: Strategy) -> None:
         self.strategies = strategies
-    
+
     def calculate_signals(self, event) -> list:
         signals = []
         if event.type == "MARKET":
             for s in self.strategies[0].bars.symbol_list:
-                strategies = []
+                strategies: list[OrderPosition] = []
                 for strategy in self.strategies:
                     strategies.append(strategy._calculate_signal(s))
-                if (all(strategies)):
-                    signals.append(strategies[0])
+        if (self.order_same_dir(strategies)):
+            signals.append(strategies[0])
         return signals
 
-class MultipleAnyStrategy(Strategy):
+
+class MultipleAnyStrategy(MultipleStrategy, Strategy):
     def __init__(self, strategies: Strategy) -> None:
         self.strategies = strategies
-    
+
     def calculate_signals(self, event) -> list:
         signals = []
         if event.type == "MARKET":
@@ -26,9 +39,11 @@ class MultipleAnyStrategy(Strategy):
                 strategies = []
                 for strategy in self.strategies:
                     strategies.append(strategy._calculate_signal(s))
-                if (any(strategies)):
-                    for strat in strategies:
-                        if strat is not None:
+                if (any(strat is not None for strat in strategies)):
+                    filtered_strat = [
+                        strat for strat in strategy if strat is not None]
+                    for strat in filtered_strat:
+                        if (self.order_same_dir(filtered_strat)):
                             signals.append(strat)
                             break
         return signals
