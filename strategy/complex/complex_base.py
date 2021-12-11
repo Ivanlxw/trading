df1b53bd320a535133c5635b4e1232f1df2d268c
Ivanlxw <leexianwei96@gmail.com>
2021-12-11 11:31:44 +0800

    add complex strat - high beta and corr with spy

---
+++ strategy/complex/complex_base.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+from trading.strategy.base import Strategy
+
+class ComplexStrategyImpl(Strategy, ABC):
+  ''' Complex Strategies
+    Differ from typical signalling strategies in the sense that there is some dependence between signals/symbols
+    Flow: 
+    - Calculate -> Generates the signals
+      - Overwrites calculate_signals, unique to each ComplexStrategy
+  '''
+  def __init__(self, bars, events, description: str = ""):
+      super().__init__(bars, events, description=description)
---
+++ strategy/complex/complex_high_beta.py
@@ -0,0 +1,67 @@
+import queue
+import numpy as np
+from typing import List, Tuple
+
+from trading.data.dataHandler import DataHandler
+from trading.event import SignalEvent
+from trading.strategy.base import Strategy
+from trading.strategy.complex.complex_base import ComplexStrategyImpl
+
+class ComplexHighBeta(ComplexStrategyImpl):
+  def __init__(self, bars: DataHandler, events: queue.Queue, index_syms: List[str], index_strategy: Strategy, corr_days:int, corr_min = 0.5, description: str = ""):
+      super().__init__(bars, events, description=description)
+      assert all(x in bars.symbol_list for x in index_syms) , "Strategy needs index to be in symbol_list"
+      assert corr_days > 25, "days to compare corr and beta has to be > 25 to be relatively robust"
+      self.index_symbols = index_syms
+      self.index_strategy = index_strategy
+      self.corr_days = corr_days
+      self.corr_min = corr_min
+      
+  # override
+  def _calculate_signal(self, _) -> List[SignalEvent]:
+    signals = []
+    for index_sym in self.index_symbols:
+      index_signal = self.index_strategy._calculate_signal(index_sym)
+      if index_signal is None or len(index_signal) == 0:
+        continue
+      assert len(index_signal) == 1, f"{index_sym} signal should be the only one"
+      index_signal = index_signal[0]
+      beta_list = self._calculate_internal()
+      if len(beta_list) == 0:
+        return []
+      # print("beta_list:  ", beta_list)
+      signals += [
+        SignalEvent(sym, index_signal.datetime, index_signal.order_position, price=price, other_details=f"ComplexHighBeta [{index_sym}]") for sym, _, price in beta_list
+      ]
+    return signals
+
+  def _calculate_internal(self) -> List[Tuple]:
+      """ Returns a list tuples of reflecting (symbol, beta) beyond a specific correlation """
+      snp_hist_prices = self.bars.get_latest_bars("SPY", self.corr_days)
+      beta_map = [("", 1, 0)]
+
+      for sym in self.bars.symbol_list:
+        if sym in self.index_symbols:
+          continue
+        sym_hist_prices = self.bars.get_latest_bars(sym, self.corr_days)
+        if len(snp_hist_prices["close"]) != len(sym_hist_prices["close"]):
+          continue
+        sym_index_corr =  np.corrcoef(snp_hist_prices["close"], sym_hist_prices["close"])
+        if sym_index_corr[0][1] < self.corr_min:
+          continue
+        spy_pct_changes = self._helper_pct_change(snp_hist_prices["close"])
+        sym_hist_changes = self._helper_pct_change(sym_hist_prices["close"])
+        try:
+          beta = np.cov(sym_hist_changes, spy_pct_changes)[0][1] / np.var(spy_pct_changes)
+        except Exception as e:
+          print(f"Input data that gave err: \n{sym}: {sym_hist_changes}\nSPY: {spy_pct_changes}")
+        if beta > 1 and beta > beta_map[-1][1]:
+          beta_map.append((sym, beta, sym_hist_prices["close"][-1]))
+      return beta_map[1:]
+
+  
+  def _helper_pct_change(self, nparray):
+    pct=np.zeros_like(nparray)
+    pct = [v + 10e-8 for v in pct]
+    pct[1:]=np.diff(nparray) / np.abs(nparray[:-1])
+    return pct
