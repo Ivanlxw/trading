458be92430a4a9883de97f4ae82edbb56f6ca20a
Ivan Lee <leexianwei96@gmail.com>
2024-02-12 22:37:11 +0800

    fix inform's plotting from recent changes in type of streaming data

--- plots/plot.py
+++ plots/plot.py
@@ -1,9 +1,10 @@
 import math
 import random
-from typing import List
+from typing import Dict, List
 
 import numpy as np
-import pandas as pd
+import pandas as pd # TODO: Remove
+import polars as pl
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.graph_objects as go
@@ -38,20 +39,18 @@ class PlotIndividual(Plot):
     """
 
     def __init__(
-        self, strategy: Strategy, signals: List[SignalEvent], plot_fair_prices: bool, historical_fair_prices: dict
+        self, signals: List[SignalEvent], historical_market_price: Dict[str, pl.DataFrame], 
+            historical_fair_price: Dict[str, pl.DataFrame] = None
     ) -> None:
         sns.set()
         sns.set_style("darkgrid")
         plt.figure(num=None, figsize=(12, 7), facecolor="w", edgecolor="k")
-        self.bars = strategy.bars
         self.signals = np.array([[sig.symbol, sig.datetime, sig.price, sig.order_position] for sig in signals])
         self.signals_cols = ["symbol", "datetime", "order_px", "order_position"]
-        self.L = min(9, len(self.bars.symbol_data.keys()))
+        self.L = min(9, len(historical_market_price))
         self.dims = self._get_squared_dims()
-        self.plot_fair_prices = plot_fair_prices
-        if self.plot_fair_prices:
-            self.historical_fair_prices = historical_fair_prices
-            assert historical_fair_prices, "historical_fair_prices is empty dict or None"
+        self.historical_market_price = historical_market_price
+        self.historical_fair_price = historical_fair_price 
 
     def _get_squared_dims(self) -> tuple:
         # if self.L == 16:
@@ -60,13 +59,13 @@ class PlotIndividual(Plot):
         return (X, math.ceil(self.L / X))
 
     def plot(self):
-        data = self.bars.latest_symbol_data
+        data = self.historical_market_price
         n_rows, n_cols = self.dims
-        tickers = random.sample(self.bars.symbol_data.keys(), self.L)
+        tickers = random.sample(self.historical_market_price.keys(), self.L)
         fig = make_subplots(rows=n_rows, cols=n_cols, subplot_titles=[ticker for ticker in tickers])
 
         for idx, ticker in enumerate(tickers):
-            obs = pd.DataFrame(data[ticker]).set_index(self.signals_cols[:2])
+            obs = data[ticker].to_pandas().set_index(self.signals_cols[:2])
             signals = (
                 pd.DataFrame(self.signals[np.where(self.signals[:, 0] == ticker)], columns=self.signals_cols)
                 .set_index(self.signals_cols[:2])
@@ -74,9 +73,10 @@ class PlotIndividual(Plot):
             )
             df = pd.merge(obs, signals, left_index=True, right_index=True, how="outer").reset_index()
             df = df.loc[~df.duplicated(), :]
-            if self.plot_fair_prices:
-                df_fair_prices = pd.DataFrame(self.historical_fair_prices[ticker])
-                df = pd.merge(df, df_fair_prices, how="left")
+            if self.historical_fair_price is not None:
+                df_fair_prices = self.historical_fair_price[ticker]
+                df = pd.merge(df, df_fair_prices.to_pandas(), how="left")
+                # df.join(self.historical_fair_price[ticker], how="left")
             row = idx // n_cols + 1
             col = idx - (row - 1) * n_cols + 1
             fig.append_trace(
@@ -98,7 +98,7 @@ class PlotIndividual(Plot):
                         x=buy_signals.index,
                         y=buy_signals.order_px,
                         mode="markers",
-                        marker=dict(line=dict(width=2, color="blue"), symbol="x"),
+                        marker=dict(line=dict(width=2, color="green"), symbol="x"),
                     ),
                     row=row,
                     col=col,
@@ -108,21 +108,23 @@ class PlotIndividual(Plot):
                         x=sell_signals.index,
                         y=sell_signals.order_px,
                         mode="markers",
-                        marker=dict(color="black", line_width=2, symbol="x"),
+                        marker=dict(color="red", line_width=2, symbol="x"),
                     ),
                     row=row,
                     col=col,
                 )
 
-            if self.plot_fair_prices:
+            if self.historical_fair_price is not None:
                 fig.append_trace(
-                    go.Scatter(x=df.index, y=df["fair_bid"], line=dict(color="yellow", width=1.5)), row=row, col=col
+                    go.Scatter(x=df.index, y=df["fair_min"], line=dict(color="red", width=1)),
+                    row=row, col=col
                 )
                 fig.append_trace(
-                    go.Scatter(x=df.index, y=df["fair_ask"], line=dict(color="orange", width=1.5)), row=row, col=col
+                    go.Scatter(x=df.index, y=df["fair_max"], line=dict(color="green", width=1)),
+                    row=row, col=col
                 )
-        min_ts = min([data[sym][0]["datetime"] for sym in tickers])
-        max_ts = max([data[sym][-1]["datetime"] for sym in tickers])
+        min_ts = min([data[sym]["datetime"][0] for sym in tickers])
+        max_ts = max([data[sym]["datetime"][-1] for sym in tickers])
         fig.update_xaxes(rangeslider_visible=False)
         fig.update_layout(showlegend=False, title_text=f"{min_ts} - {max_ts}")
         fig.show()
